056dedcebd8e8a7baa3a642fa4df78f6a5975611
Max Cliford JEAN <maxclifordjean@gmail.com>
2013-06-12 12:21:44 +0200

    add an uploader to a model "paintings"

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130612095958) do
+ActiveRecord::Schema.define(:version => 20130612101259) do
 
   create_table "addresses", :force => true do |t|
     t.integer "adress_id"
@@ -113,6 +113,7 @@ ActiveRecord::Schema.define(:version => 20130612095958) do
     t.text     "description"
     t.datetime "created_at",  :null => false
     t.datetime "updated_at",  :null => false
+    t.string   "image"
   end
 
   create_table "pictures", :force => true do |t|
